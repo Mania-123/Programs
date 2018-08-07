1eb933c732f93fb8ad6415125d703a260ff9f24c
Vishal Varma <vishal.varma919@outlook.com>
2018-08-07 17:39:59 +0530

    Insertion into linked list , Some steps are pending I will do latter

---
+++ vishal.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+/* Demenstration of linked list */
+
+struct node   /* creating node*/
+{
+	int i;
+	struct node *next;
+};
+
+
+
+int insert_at_begin(int data,struct node *head)
+{
+	
+	
+	struct node *new=(struct node *)malloc(sizeof(struct node));
+	new->next=head;
+	head=new;
+}
+
+int insert_at_end(int data,struct node *head)
+{
+	struct node *new=(struct node *)malloc(sizeof(struct node));
+	struct node *temp=head;
+	
+	while(temp->next)
+	{
+		temp=temp->next;
+			
+	}
+
+	temp->next=new;
+	new->next='\0';
+	
+}
+
+int main()
+{
+	char c;
+	int d;
+	struct node *head='\0';
+	head=(struct node *)malloc(sizeof(struct node));
+	printf("choose B to insert at begining or choose E to insert at end\n");
+	scanf("%c",&c);
+
+	switch(c)
+	case 'B': 
+	printf("enter the data value \n",d);
+	scanf("%d",&d);
+	insert_at_begin(d,head);
+}
